2401da3d075333f8c4333938bbf7bd4fc0748559
Evaldas <evka85@gmail.com>
2016-03-16 02:21:40 +0100

    minor version increase due to tx protocol change

--- src/system/sys/system_version_package.vhd
+++ src/system/sys/system_version_package.vhd
@@ -3,8 +3,8 @@ use ieee.std_logic_1164.all;
 package system_version_package is
 	
 	constant sys_ver_major:integer range 0 to 15 :=2;
-	constant sys_ver_minor:integer range 0 to 15 :=2;
-	constant sys_ver_build:integer range 0 to 255:=7;
+	constant sys_ver_minor:integer range 0 to 15 :=3;
+	constant sys_ver_build:integer range 0 to 255:=0;
 	constant sys_ver_year :integer range 0 to 99 :=16;
 	constant sys_ver_month:integer range 0 to 12 :=3;
 	constant sys_ver_day  :integer range 0 to 31 :=15;
